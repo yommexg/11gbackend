ddd886997c8d8b6527b81f8dd148d44814e30df4
yommexg <hydrokidb@gmail.com>
2024-05-24 02:46:11 +0100

    update

--- routes/usedCars.js
+++ routes/usedCars.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usedCarController = require("../controllers/usedCarController");
 
 router.get("/", usedCarController.getAllApprovedUsedCars);
+router.get("/:usedCarId", usedCarController.getUsedCar);
 
 module.exports = router;
