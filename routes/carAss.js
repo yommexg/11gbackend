8cf0d4b95123e186da8d9b18726710f60550d8dd
yommexg <hydrokidb@gmail.com>
2024-05-28 03:09:47 +0100

    update

--- routes/carAss.js
+++ routes/carAss.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const carAssController = require("../controllers/carAssController");
 
 router.get("/", carAssController.getAllApprovedCarAss);
+router.get("/:itemId", carAssController.getCarAss);
 
 module.exports = router;
