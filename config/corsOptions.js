6107a6b16de7bad9714c3822a57733cdf00d0d06
yommexg <hydrokidb@gmail.com>
2024-06-13 04:32:00 +0100

    update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,20 @@
 const allowedOrigins = require("./allowedOrigins");
 
+const isAllowedOrigin = (origin) => {
+  // Check if the origin is in the allowedOrigins array
+  return allowedOrigins.includes(origin);
+};
+
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
+    if (!origin || !isAllowedOrigin(origin)) {
+      // Reject requests with disallowed origins
       callback(new Error("Not allowed by CORS"));
+    } else {
+      callback(null, true);
     }
   },
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
   credentials: true,
 };
 
